b3ccd8c4299e3b03c31f77c46239549776f651a7
Sebastian Fiedlschuster <git@fiedlschuster.de>
2023-05-22 16:25:09 +0200

    initial commit (bundle gem matestack-renderer)

---
+++ lib/matestack/renderer.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "renderer/version"
+
+module Matestack
+  module Renderer
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
